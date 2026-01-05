bf3f03086247681dd17d0addd68892d0d75d27a3
Muhammad Moeen <mooenmalik374@gmail.com>
2026-01-06 00:08:16 +0500

    Implemented the logic for the user to register,login,to get verified and to logout in the controllers also added the user model and sessions to keep user's session for a week.

--- server/server.ts
+++ server/server.ts
@@ -2,14 +2,42 @@ import express, { Request, Response } from 'express';
 import cors from "cors"
 import "dotenv/config"
 import { connectDB } from './config/db.js';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import AuthRouter from './routes/authRoutes.js';
+declare module "express-session" {
+    interface SessionData {
+        isLoggedIn: boolean,
+        userId: string
+    }
+}
+
+
 
 await connectDB();
 const app = express();
 
 
 //! middlewares
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173", "http://localhost:3000"],
+    credentials: true,
+}));
+
+app.use(session({
+    secret: process.env.SESSION_SECRETKEY as string,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 },
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI as string,
+        collectionName: "sessions"
+    })
+}))
 app.use(express.json());
+
+//! AuthRoutes
+app.use("/api/auth",AuthRouter);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello!');
