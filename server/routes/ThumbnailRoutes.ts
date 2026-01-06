7568b8c297671c9cad1a65aa338f914810ca8d1d
Muhammad Moeen <mooenmalik374@gmail.com>
2026-01-06 20:33:27 +0500

    Protected to the thumbnail and user routes which are only accesible when the user is logged in.

--- server/routes/ThumbnailRoutes.ts
+++ server/routes/ThumbnailRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { delThumbnail, generateThumbnail } from "../controllers/ThumbnailController.js";
+import authMiddleware from "../middlewares/auth.js";
 
 const ThumbnailRouter = express.Router()
 
-ThumbnailRouter.post("/generate",generateThumbnail);
-ThumbnailRouter.delete("/delete/:id",delThumbnail);
+ThumbnailRouter.post("/generate", authMiddleware, generateThumbnail);
+ThumbnailRouter.delete("/delete/:id", authMiddleware, delThumbnail);
 
 
 export default ThumbnailRouter;
--- server/routes/UserRoutes.ts
+++ server/routes/UserRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express"
 import { getThumbnailById, getUersThumbnails } from "../controllers/UserController.js";
+import authMiddleware from "../middlewares/auth.js";
 
 const UserRouter = express.Router();
 
-UserRouter.get("/thumbnails",getUersThumbnails);
-UserRouter.get("/thumbnail/:id",getThumbnailById);
+UserRouter.get("/thumbnails", authMiddleware, getUersThumbnails);
+UserRouter.get("/thumbnail/:id", authMiddleware, getThumbnailById);
 
 export default UserRouter;
